18876f443c9b24f575f4c42937ec40f79065166f
Lavric Cosmin-Marian <cosmin.lavric@student.unitbv.ro>
2024-10-28 21:12:34 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -131,12 +131,31 @@ const App: React.FC = () => {
   }, [profitPerHour]);
 
   return (
+<<<<<<< Updated upstream
     <div className="bg-black flex justify-center">
       <div className="w-full bg-black text-white h-screen font-bold flex flex-col max-w-xl">
         <div className="px-4 z-10">
           <div className="flex items-center space-x-2 pt-4">
             <div className="p-1 rounded-lg bg-[#1d2025]">
               <Hamster size={24} className="text-[#d4d4d4]" />
+=======
+    <>
+      <div className="bg-black flex justify-center">
+        <div className="w-full bg-black text-white h-screen font-bold flex flex-col max-w-xl">
+          <div className="px-4 z-10">
+            {/*******************************************************
+             *                                                     *
+             *             Top Section of the Application          *
+             *                                                     *
+             *******************************************************/}
+            <div className="flex items-center space-x-2 pt-4">
+              <div className="p-1 rounded-lg bg-[#1d2025]">
+                <Hamster size={24} className="text-[#d4d4d4]" />
+              </div>
+              <div>
+                <p className="text-sm">cosminmarian53</p>
+              </div>
+>>>>>>> Stashed changes
             </div>
             <div>
               <p className="text-sm">Nikandr (CEO)</p>
