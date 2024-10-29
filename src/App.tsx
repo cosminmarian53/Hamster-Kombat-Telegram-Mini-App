6314c5a18099415689114e84feeafe85a21fcecb
Lavric Cosmin-Marian <cosmin.lavric@student.unitbv.ro>
2024-10-29 17:46:23 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,53 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import Hamster from './icons/Hamster';
-import { binanceLogo, dailyCipher, dailyCombo, dailyReward, dollarCoin, hamsterCoin, mainCharacter } from './images';
-import Info from './icons/Info';
-import Settings from './icons/Settings';
-import Mine from './icons/Mine';
-import Friends from './icons/Friends';
-import Coins from './icons/Coins';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Hamster from "./icons/Hamster";
+import {
+  binanceLogo,
+  dailyCipher,
+  dailyCombo,
+  dailyReward,
+  dollarCoin,
+  hamsterCoin,
+  mainCharacter,
+} from "./images";
+import Info from "./icons/Info";
+import Settings from "./icons/Settings";
+import Mine from "./icons/Mine";
+import Friends from "./icons/Friends";
+import Coins from "./icons/Coins";
 
 const App: React.FC = () => {
   const levelNames = [
-    "Bronze",    // From 0 to 4999 coins
-    "Silver",    // From 5000 coins to 24,999 coins
-    "Gold",      // From 25,000 coins to 99,999 coins
-    "Platinum",  // From 100,000 coins to 999,999 coins
-    "Diamond",   // From 1,000,000 coins to 2,000,000 coins
-    "Epic",      // From 2,000,000 coins to 10,000,000 coins
+    "Bronze", // From 0 to 4999 coins
+    "Silver", // From 5000 coins to 24,999 coins
+    "Gold", // From 25,000 coins to 99,999 coins
+    "Platinum", // From 100,000 coins to 999,999 coins
+    "Diamond", // From 1,000,000 coins to 2,000,000 coins
+    "Epic", // From 2,000,000 coins to 10,000,000 coins
     "Legendary", // From 10,000,000 coins to 50,000,000 coins
-    "Master",    // From 50,000,000 coins to 100,000,000 coins
+    "Master", // From 50,000,000 coins to 100,000,000 coins
     "GrandMaster", // From 100,000,000 coins to 1,000,000,000 coins
-    "Lord"       // From 1,000,000,000 coins to ∞
+    "Lord", // From 1,000,000,000 coins to ∞
   ];
 
   const levelMinPoints = [
-    0,        // Bronze
-    5000,     // Silver
-    25000,    // Gold
-    100000,   // Platinum
-    1000000,  // Diamond
-    2000000,  // Epic
+    0, // Bronze
+    5000, // Silver
+    25000, // Gold
+    100000, // Platinum
+    1000000, // Diamond
+    2000000, // Epic
     10000000, // Legendary
     50000000, // Master
-    100000000,// GrandMaster
-    1000000000// Lord
+    100000000, // GrandMaster
+    1000000000, // Lord
   ];
 
   const [levelIndex, setLevelIndex] = useState(6);
   const [points, setPoints] = useState(22749365);
-  const [clicks, setClicks] = useState<{ id: number, x: number, y: number }[]>([]);
+  const [clicks, setClicks] = useState<{ id: number; x: number; y: number }[]>(
+    []
+  );
   const pointsToAdd = 11;
   const profitPerHour = 126420;
 
@@ -58,8 +68,8 @@ const App: React.FC = () => {
     const hours = Math.floor(diff / (1000 * 60 * 60));
     const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
 
-    const paddedHours = hours.toString().padStart(2, '0');
-    const paddedMinutes = minutes.toString().padStart(2, '0');
+    const paddedHours = hours.toString().padStart(2, "0");
+    const paddedMinutes = minutes.toString().padStart(2, "0");
 
     return `${paddedHours}:${paddedMinutes}`;
   };
@@ -82,9 +92,11 @@ const App: React.FC = () => {
     const rect = card.getBoundingClientRect();
     const x = e.clientX - rect.left - rect.width / 2;
     const y = e.clientY - rect.top - rect.height / 2;
-    card.style.transform = `perspective(1000px) rotateX(${-y / 10}deg) rotateY(${x / 10}deg)`;
+    card.style.transform = `perspective(1000px) rotateX(${
+      -y / 10
+    }deg) rotateY(${x / 10}deg)`;
     setTimeout(() => {
-      card.style.transform = '';
+      card.style.transform = "";
     }, 100);
 
     setPoints(points + pointsToAdd);
@@ -92,7 +104,7 @@ const App: React.FC = () => {
   };
 
   const handleAnimationEnd = (id: number) => {
-    setClicks((prevClicks) => prevClicks.filter(click => click.id !== id));
+    setClicks((prevClicks) => prevClicks.filter((click) => click.id !== id));
   };
 
   const calculateProgress = () => {
@@ -101,7 +113,8 @@ const App: React.FC = () => {
     }
     const currentLevelMin = levelMinPoints[levelIndex];
     const nextLevelMin = levelMinPoints[levelIndex + 1];
-    const progress = ((points - currentLevelMin) / (nextLevelMin - currentLevelMin)) * 100;
+    const progress =
+      ((points - currentLevelMin) / (nextLevelMin - currentLevelMin)) * 100;
     return Math.min(progress, 100);
   };
 
@@ -125,37 +138,18 @@ const App: React.FC = () => {
   useEffect(() => {
     const pointsPerSecond = Math.floor(profitPerHour / 3600);
     const interval = setInterval(() => {
-      setPoints(prevPoints => prevPoints + pointsPerSecond);
+      setPoints((prevPoints) => prevPoints + pointsPerSecond);
     }, 1000);
     return () => clearInterval(interval);
   }, [profitPerHour]);
 
   return (
-<<<<<<< Updated upstream
     <div className="bg-black flex justify-center">
       <div className="w-full bg-black text-white h-screen font-bold flex flex-col max-w-xl">
         <div className="px-4 z-10">
           <div className="flex items-center space-x-2 pt-4">
             <div className="p-1 rounded-lg bg-[#1d2025]">
               <Hamster size={24} className="text-[#d4d4d4]" />
-=======
-    <>
-      <div className="bg-black flex justify-center">
-        <div className="w-full bg-black text-white h-screen font-bold flex flex-col max-w-xl">
-          <div className="px-4 z-10">
-            {/*******************************************************
-             *                                                     *
-             *             Top Section of the Application          *
-             *                                                     *
-             *******************************************************/}
-            <div className="flex items-center space-x-2 pt-4">
-              <div className="p-1 rounded-lg bg-[#1d2025]">
-                <Hamster size={24} className="text-[#d4d4d4]" />
-              </div>
-              <div>
-                <p className="text-sm">cosminmarian53</p>
-              </div>
->>>>>>> Stashed changes
             </div>
             <div>
               <p className="text-sm">Nikandr (CEO)</p>
@@ -166,11 +160,19 @@ const App: React.FC = () => {
               <div className="w-full">
                 <div className="flex justify-between">
                   <p className="text-sm">{levelNames[levelIndex]}</p>
-                  <p className="text-sm">{levelIndex + 1} <span className="text-[#95908a]">/ {levelNames.length}</span></p>
+                  <p className="text-sm">
+                    {levelIndex + 1}{" "}
+                    <span className="text-[#95908a]">
+                      / {levelNames.length}
+                    </span>
+                  </p>
                 </div>
                 <div className="flex items-center mt-1 border-2 border-[#43433b] rounded-full">
                   <div className="w-full h-2 bg-[#43433b]/[0.6] rounded-full">
-                    <div className="progress-gradient h-2 rounded-full" style={{ width: `${calculateProgress()}%` }}></div>
+                    <div
+                      className="progress-gradient h-2 rounded-full"
+                      style={{ width: `${calculateProgress()}%` }}
+                    ></div>
                   </div>
                 </div>
               </div>
@@ -179,10 +181,18 @@ const App: React.FC = () => {
               <img src={binanceLogo} alt="Exchange" className="w-8 h-8" />
               <div className="h-[32px] w-[2px] bg-[#43433b] mx-2"></div>
               <div className="flex-1 text-center">
-                <p className="text-xs text-[#85827d] font-medium">Profit per hour</p>
+                <p className="text-xs text-[#85827d] font-medium">
+                  Profit per hour
+                </p>
                 <div className="flex items-center justify-center space-x-1">
-                  <img src={dollarCoin} alt="Dollar Coin" className="w-[18px] h-[18px]" />
-                  <p className="text-sm">{formatProfitPerHour(profitPerHour)}</p>
+                  <img
+                    src={dollarCoin}
+                    alt="Dollar Coin"
+                    className="w-[18px] h-[18px]"
+                  />
+                  <p className="text-sm">
+                    {formatProfitPerHour(profitPerHour)}
+                  </p>
                   <Info size={20} className="text-[#43433b]" />
                 </div>
               </div>
@@ -197,21 +207,45 @@ const App: React.FC = () => {
             <div className="px-4 mt-6 flex justify-between gap-2">
               <div className="bg-[#272a2f] rounded-lg px-4 py-2 w-full relative">
                 <div className="dot"></div>
-                <img src={dailyReward} alt="Daily Reward" className="mx-auto w-12 h-12" />
-                <p className="text-[10px] text-center text-white mt-1">Daily reward</p>
-                <p className="text-[10px] font-medium text-center text-gray-400 mt-2">{dailyRewardTimeLeft}</p>
+                <img
+                  src={dailyReward}
+                  alt="Daily Reward"
+                  className="mx-auto w-12 h-12"
+                />
+                <p className="text-[10px] text-center text-white mt-1">
+                  Daily reward
+                </p>
+                <p className="text-[10px] font-medium text-center text-gray-400 mt-2">
+                  {dailyRewardTimeLeft}
+                </p>
               </div>
               <div className="bg-[#272a2f] rounded-lg px-4 py-2 w-full relative">
                 <div className="dot"></div>
-                <img src={dailyCipher} alt="Daily Cipher" className="mx-auto w-12 h-12" />
-                <p className="text-[10px] text-center text-white mt-1">Daily cipher</p>
-                <p className="text-[10px] font-medium text-center text-gray-400 mt-2">{dailyCipherTimeLeft}</p>
+                <img
+                  src={dailyCipher}
+                  alt="Daily Cipher"
+                  className="mx-auto w-12 h-12"
+                />
+                <p className="text-[10px] text-center text-white mt-1">
+                  Daily cipher
+                </p>
+                <p className="text-[10px] font-medium text-center text-gray-400 mt-2">
+                  {dailyCipherTimeLeft}
+                </p>
               </div>
               <div className="bg-[#272a2f] rounded-lg px-4 py-2 w-full relative">
                 <div className="dot"></div>
-                <img src={dailyCombo} alt="Daily Combo" className="mx-auto w-12 h-12" />
-                <p className="text-[10px] text-center text-white mt-1">Daily combo</p>
-                <p className="text-[10px] font-medium text-center text-gray-400 mt-2">{dailyComboTimeLeft}</p>
+                <img
+                  src={dailyCombo}
+                  alt="Daily Combo"
+                  className="mx-auto w-12 h-12"
+                />
+                <p className="text-[10px] text-center text-white mt-1">
+                  Daily combo
+                </p>
+                <p className="text-[10px] font-medium text-center text-gray-400 mt-2">
+                  {dailyComboTimeLeft}
+                </p>
               </div>
             </div>
 
@@ -228,7 +262,11 @@ const App: React.FC = () => {
                 onClick={handleCardClick}
               >
                 <div className="w-full h-full rounded-full circle-inner">
-                  <img src={mainCharacter} alt="Main Character" className="w-full h-full" />
+                  <img
+                    src={mainCharacter}
+                    alt="Main Character"
+                    className="w-full h-full"
+                  />
                 </div>
               </div>
             </div>
@@ -267,7 +305,7 @@ const App: React.FC = () => {
           style={{
             top: `${click.y - 42}px`,
             left: `${click.x - 28}px`,
-            animation: `float 1s ease-out`
+            animation: `float 1s ease-out`,
           }}
           onAnimationEnd={() => handleAnimationEnd(click.id)}
         >
